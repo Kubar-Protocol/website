09c27334faac92410a229fda1089152cdad2d8a7
Oshaib Beg <oshaibbeg181203@gmail.com>
2025-05-01 18:56:26 +0530

    KUP-20 branch

---
+++ kubar-landing-page/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+      <nav className="flex justify-between items-center px-6 py-4 bg-black text-white">
+        <a href="#" className="text-sm underline">MSME Resource Hub</a>
+        <button className="border px-4 py-1 rounded">Login</button>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
